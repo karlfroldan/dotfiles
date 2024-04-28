395a949bd02c05ba18efde143a583b30d6d94b1a
karlfroldan <karlfroldan@proton.me>
2024-04-28 08:34:48 +0800

    removed emacs because I use PCs without hyprland installed

--- emacs/encryption.el
+++
@@ -1,8 +0,0 @@
-(require 'epa)
-
-(setenv "GPA_AGENT_INFO" nil)
-
-(setq karl/gpg-key-email-address "karlfroldan@gmail.com")
-
-(setq-default epa-file-select-keys '(karl/gpg-key-email-address))
-(setq epa-file-encrypt-to karl/gpg-key-email-address)
--- emacs/keybindings.el
+++
@@ -1,3 +0,0 @@
-;; Single-line scrolling
-(global-set-key (kbd "C-.") 'scroll-up-line)
-(global-set-key (kbd "C-,") 'scroll-down-line)
