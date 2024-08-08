ef62acb5365520a15d2cd3c2a669546e30fa9d73
karlfroldan <karlfroldan@proton.me>
2024-08-08 16:21:08 +0800

    additional updates

--- ags/config.js
+++ ags/config.js
@@ -1,182 +1,231 @@
-const hyprland = await Service.import('hyprland')
-const battery = await Service.import('battery')
-const network = await Service.import('network')
-const audio = await Service.import('audio')
-import { CalendarPopup } from './calendar_popup.js'
-import { PowerButton } from './power.js'
+const hyprland = await Service.import("hyprland")
+const notifications = await Service.import("notifications")
+const mpris = await Service.import("mpris")
+const audio = await Service.import("audio")
+const battery = await Service.import("battery")
+const systemtray = await Service.import("systemtray")
 
-const date = Variable('', {
-    poll: [1000, 'date "+%H:%M, %b %e, %Y"'],
+const date = Variable("", {
+    poll: [1000, 'date "+%H:%M:%S %b %e."'],
 })
 
-const Clock = (monitor = 0) => {
-    const calendar_name = `calendarPopup${monitor}`
-    const calendar_popup = CalendarPopup(calendar_name)
+function truncate_string(str, n) {
+    if (str.length < n) {
+        return str
+    } else {
+        return str.slice(0, n - 3) + '...'
+    }
+}
 
-    App.addWindow(calendar_popup);
-    // Window is initially closed
-    App.closeWindow(calendar_name);
-    
-    return Widget.Button({
-        child: Widget.Label({
-            label: date.bind(),
-        }),
-        onClicked: () => App.toggleWindow(calendar_name),
+// widgets can be only assigned as a child in one container
+// so to make a reuseable widget, make it a function
+// then you can simply instantiate one by calling it
+
+function Workspaces() {
+    const activeId = hyprland.active.workspace.bind("id")
+    const workspaces = hyprland.bind("workspaces")
+        .as(ws => ws.map(({ id }) => Widget.Button({
+            on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
+            child: Widget.Label(`${id}`),
+            class_name: activeId.as(i => `${i === id ? "focused" : ""}`),
+        })))
+
+    return Widget.Box({
+        class_name: "workspaces",
+        children: workspaces,
     })
 }
 
 
-/// The RevealerButton will have a button and an icon that may change given the
-/// status of the sliding bar.
-const RevealerButton = (
-    slider_fn, // function that will execute onChange of slider
-    slider_value, // value of the slider.
-    icon = 'emblem-default-symbolic',
-    icon_hook = () => {},
-) => {
-    const is_revealed = Variable(false)
-
-    const button_icon = Widget.Icon({icon})
-    // Clicking the button will disable the reveal of the slider.
-    const button = Widget.Button({
-        child: button_icon,
-        onHover: () => {
-            is_revealed.value = true
-        },
-        onClicked: () => {
-            is_revealed.value = !is_revealed.value
-        },
+function ClientTitle() {
+    return Widget.Label({
+        class_name: "client-title",
+        label: hyprland.active.client.bind("title"),
     })
+}
 
-    // This is the child of the revealer
-    const slider = Widget.Slider({
-        vertical: false,
-        draw_value: false,
-        hexpand: true,
-        value: slider_value,
-        min: 0,
-        max: 1,
-        onChange: slider_fn,
+
+function Clock() {
+    return Widget.Label({
+        class_name: "clock",
+        label: date.bind(),
     })
+}
 
-    const revealer = Widget.Revealer({
-        revealChild: is_revealed.bind(),
-        transitionDuration: 1000,
-        transition: 'slide_right',
-        child: slider,
-    })
 
+// we don't need dunst or any other notification daemon
+// because the Notifications module is a notification daemon itself
+function Notification() {
+    const popups = notifications.bind("popups")
     return Widget.Box({
-        spacing: 2,
-        vertical: false,
-        css: 'min-width: 180px',
+        class_name: "notification",
+        visible: popups.as(p => p.length > 0),
         children: [
-            button,
-            revealer,
+            Widget.Icon({
+                icon: "preferences-system-notifications-symbolic",
+            }),
+            Widget.Label({
+                label: popups.as(p => p[0]?.summary || ""),
+            }),
         ],
     })
 }
 
-/// My laptop currently only has Wi-Fi so let's only consider that for now.
-const NetworkCenter = () => {
+
+
+function Media() {
+    const label = Utils.watch("", mpris, "player-changed", () => {
+        if (mpris.players[0]) {
+            const { track_artists, track_title } = mpris.players[0]
+            // length threshold to determine when to cut off the title
+            const threshold = 40
+            const relabel = `${track_artists.join(", ")} - ${track_title}`
+            return truncate_string(relabel, threshold)
+        } else {
+            return "Nothing is playing"
+        }
+    })
+
+    return Widget.Button({
+        class_name: "media",
+        on_primary_click: () => mpris.getPlayer("")?.playPause(),
+        on_scroll_up: () => mpris.getPlayer("")?.next(),
+        on_scroll_down: () => mpris.getPlayer("")?.previous(),
+        child: Widget.Label({ label }),
+    })
+}
+
+
+function Volume() {
+    const icons = {
+        101: "overamplified",
+        67: "high",
+        34: "medium",
+        1: "low",
+        0: "muted",
+    }
+
+    function getIcon() {
+        const icon = audio.speaker.is_muted ? 0 : [101, 67, 34, 1, 0].find(
+            threshold => threshold <= audio.speaker.volume * 100)
+
+        return `audio-volume-${icons[icon]}-symbolic`
+    }
+
+    const icon = Widget.Icon({
+        icon: Utils.watch(getIcon(), audio.speaker, getIcon),
+    })
+
+    const slider = Widget.Slider({
+        hexpand: true,
+        draw_value: false,
+        on_change: ({ value }) => audio.speaker.volume = value,
+        setup: self => self.hook(audio.speaker, () => {
+            self.value = audio.speaker.volume || 0
+        }),
+    })
+
+    return Widget.Box({
+        class_name: "volume",
+        css: "min-width: 180px",
+        children: [icon, slider],
+    })
+}
+
+
+function BatteryLabel() {
+    const value = battery.bind("percent").as(p => p > 0 ? p / 100 : 0)
+    const icon = battery.bind("percent").as(p =>
+        `battery-level-${Math.floor(p / 10) * 10}-symbolic`)
+
+    return Widget.Box({
+        class_name: "battery",
+        visible: battery.bind("available"),
+        children: [
+            Widget.Icon({ icon }),
+            Widget.LevelBar({
+                widthRequest: 140,
+                vpack: "center",
+                value,
+            }),
+        ],
+    })
+}
+
+
+function SysTray() {
+    const items = systemtray.bind("items")
+        .as(items => items.map(item => Widget.Button({
+            child: Widget.Icon({ icon: item.bind("icon") }),
+            on_primary_click: (_, event) => item.activate(event),
+            on_secondary_click: (_, event) => item.openMenu(event),
+            tooltip_markup: item.bind("tooltip_markup"),
+        })))
+
+    return Widget.Box({
+        children: items,
+    })
+}
+
+
+// layout of the bar
+function Left() {
     return Widget.Box({
         spacing: 8,
-        vertical: false,
         children: [
-            Widget.Label({
-                label: Utils.merge([
-                    network.wifi.bind('internet'), network.wifi.bind('ssid'),
-                ], (wireless_status, ssid) => {
-                    if (wireless_status != 'disconnected')
-                        return ssid
-                    else
-                        return 'Not connected'
-                }),
-            }),
-            Widget.Icon({
-                icon: network.wifi.bind('icon-name')
-            }),
+            Workspaces(),
+            ClientTitle(),
         ],
     })
 }
 
-/// For the battery progress, I prefer that it shows the battery status
-/// alongside the actual percentage so let's use a Box with a horizontal orientation.
-const BatteryProgress = () => {
+function Center() {
     return Widget.Box({
-        spacing: 1,
-        vertical: false,
+        spacing: 8,
         children: [
-            Widget.Label({
-                label: battery.bind('percent').as(p => String(p) + '%'),
-            }),
-            Widget.Icon({
-                icon: battery.bind('icon-name'),
-            }),
-        ]
+            Media(),
+            Notification(),
+        ],
     })
 }
 
-const AudioBar = () => {
-    return RevealerButton(
-        ({value}) => {
-            audio['speaker'].volume = value
-        },
-        audio['speaker'].bind('volume'),
-        'audio-speakers-symbolic'
-    )
-}
-
-const Left = () => {
-    return Widget.Label('Left Widget')
-}
-
-const Center = () => {
+function Right() {
     return Widget.Box({
-        hpack: 'center',
-        spacing: 64,
-        vertical: false,
+        hpack: "end",
+        spacing: 8,
         children: [
+            Volume(),
+            BatteryLabel(),
             Clock(),
+            SysTray(),
         ],
     })
 }
 
-const Right = () => {
-    return Widget.Box({
-        hpack: 'center',
-        spacing: 64, 
-        homogeneous: false,
-        vertical: false,
-        children: [
-            BatteryProgress(),
-            NetworkCenter(),
-            AudioBar(),
-            PowerButton(),
-        ],
-    })
-}
-
-const Bar = (monitor = 0) => {
+function Bar(monitor = 0) {
     return Widget.Window({
+        name: `bar-${monitor}`, // name has to be unique
+        class_name: "bar",
         monitor,
-        name: `bar${monitor}`,
-        layer: 'top',
-        anchor: ['top', 'left', 'right'],
+        anchor: ["top", "left", "right"],
+        exclusivity: "exclusive",
         child: Widget.CenterBox({
-            vertical: false,
-            startWidget: Left(),
-            centerWidget: Center(),
-            endWidget: Right(),
+            start_widget: Left(),
+            center_widget: Center(),
+            end_widget: Right(),
         }),
-        // date +'%a, %k:%M %m/%H/%Y'
-    });
-};
+    })
+}
 
 App.config({
-    style: './style.css',
+    style: "./style.css",
     windows: [
-        // This is where the window definitions will go.
-        Bar()
-    ]
-});
+        Bar(),
+
+        // you can call it, for each monitor
+        // Bar(0),
+        // Bar(1)
+    ],
+})
+
+export { }
